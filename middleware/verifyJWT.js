c6d1eb89fd3e2f2b83d075483bef52326f37bda1
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2022-08-21 23:41:52 +0600

    creating verifyJWT Middleware

---
+++ middleware/verifyJWT.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+const secret = process.env.JWTKEY;
+
+const verifyJWT = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ Message: "UnAuthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, secret, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ Message: "Forbidden access" });
+    }
+
+    req.user = decoded?.id;
+    next();
+  });
+};
+
+export default verifyJWT;
